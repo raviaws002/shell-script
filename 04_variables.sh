6577a89d4de4a9c260ea16e95f179b0555aa4a38
ravisripada <krishnaravikirans02@gmail.com>
2024-10-22 23:04:02 +0530

    variables added

---
+++ 04_variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+PERSON_1=$1
+PERSON_2=$2
+
+echo "$Person_1: How are you!"
+echo "$Person_2: Fine! I am doing good. What are you doing?"
+echo "$Person_1: I am working as a SOftware Engineer."
