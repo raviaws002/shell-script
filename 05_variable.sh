13c4736af86e96f3b6aa144927e4da9ed48e194b
ravisripada <krishnaravikirans02@gmail.com>
2024-10-22 23:58:02 +0530

    changes made

--- 05_variable.sh
+++ 05_variable.sh
@@ -1,7 +1,28 @@
 #!bin/bash
 
-USERNAME=$1
-PASSWD=$2
 
-echo "Username is: $USERNAME"
-echo "Passwd is: $PASSWD"
+#USERNAME=$1
+#PASSWD=$2
+
+#echo "Username is: $USERNAME"
+#echo "Passwd is: $PASSWD" 
+----------------------------------------------------------------------
+
+
+
+#!/bin/bash
+
+# Prompt for the username
+echo "Enter the Username:: "
+read -s USERNAME  # Read username input
+
+# Prompt for the password
+echo "Enter the Password:: "
+read -s PASSWD  # Read password silently
+
+# Display the username and password (be cautious about displaying passwords)
+echo "Username is: $USERNAME, Password is: $PASSWD"
+
+
+
+
