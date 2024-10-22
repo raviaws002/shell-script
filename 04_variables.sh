ce1c3f5f9e633c1e3e9bcf3384ba2fd14871d99a
ravisripada <krishnaravikirans02@gmail.com>
2024-10-22 23:09:41 +0530

    add changes

--- 04_variables.sh
+++ 04_variables.sh
@@ -7,3 +7,4 @@ PERSON_2=$2
 echo "$Person_1: How are you!"
 echo "$Person_2: Fine! I am doing good. What are you doing?"
 echo "$Person_1: I am working as a SOftware Engineer."
+echo "$Person_2: Very Nice! Hope you really doing well"
